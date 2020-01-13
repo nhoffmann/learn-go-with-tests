a070f126c3c29b0c9b11bbae0b53607694d3c8b0
Niels Hoffmann <niels@zentralmaschine.net>
2020-01-13 12:51:37 +0100

    Refactor for multi language support

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,10 +3,45 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Niels")
-	want := "Hello, Niels"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	asserCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Niels", "")
+		want := "Hello, Niels"
+
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Jose", "Spanish")
+		want := "Hola, Jose"
+
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Claude", "French")
+		want := "Bonjour, Claude"
+
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in German", func(t *testing.T) {
+		got := Hello("Klaus", "German")
+		want := "Hallo, Klaus"
+
+		asserCorrectMessage(t, got, want)
+	})
 }
